de75dc05be5695c8e8f3f000d5014a429bb73fa3
nicholasburgher <55260105+nicholasburgher@users.noreply.github.com>
2020-06-18 16:58:20 -0500

    more design work
    Added in additional elements and made major updated to the form portion of the modal

--- js/index.js
+++ js/index.js
@@ -5,3 +5,27 @@ function flip(targetID) {
   cards[targetID].classList.toggle("flipped");
   deviceGrid.classList.toggle("off");
 }
+
+window.onload = function() {
+  document.querySelector(".container-fluid").classList.add("show");
+}
+
+function validateForm(form) {
+  console.log(form);
+  var color = document.forms[form]["colors"].value;
+  var capacity = document.forms[form]["capacity"].value;
+  var carrier = document.forms[form]["carrier"].value;
+  if (color == "") {
+    alert("You must select a color");
+    return false;
+  }
+  if (capacity == "") {
+    alert("You must select a capacity");
+    return false;
+  }
+  if (carrier == "") {
+    alert("You must select a carrier");
+    return false;
+  }
+  document.forms[form].setAttribute("action","index.html?color=" + color + "&capacity=" + capacity + "&carrier=" + carrier );
+}
